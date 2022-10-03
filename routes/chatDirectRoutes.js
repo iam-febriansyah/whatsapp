dc5efebb58d730ceb91b5ef79e4213cdd47dec8d
Febriansyah <febriansyah032@gmail.com>
2022-10-03 21:11:03 +0700

    update git

--- routes/chatDirectRoutes.js
+++ routes/chatDirectRoutes.js
@@ -3,8 +3,8 @@ import * as controller from '../controllers/chatDirectController.js'
 
 const router = Router()
 
-router.post('/send-direct-text', controller.sendText)
-router.post('/send-direct-document', controller.sendDoc)
-router.post('/send-direct-image', controller.sendImage)
+router.post('/send-direct-text', controller.sendDirectText)
+router.post('/send-direct-document', controller.sendDirectDoc)
+router.post('/send-direct-image', controller.sendDirectImage)
 
 export default router
