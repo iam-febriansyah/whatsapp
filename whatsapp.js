a655d61ec7c0598b1de32deacb5369f265cb3bf2
Febriansyah <febriansyah032@gmail.com>
2022-10-03 21:36:23 +0700

    update git

--- whatsapp.js
+++ whatsapp.js
@@ -1,328 +1,354 @@
-import { existsSync, unlinkSync, readdir } from "fs";
-import { join } from "path";
-import makeWASocket, { makeWALegacySocket, useSingleFileAuthState, useSingleFileLegacyAuthState, makeInMemoryStore, Browsers, DisconnectReason, delay } from "@adiwajshing/baileys";
-import { toDataURL } from "qrcode";
-import __dirname from "./dirname.js";
-import response from "./response.js";
-import db from "./models/index.js";
-import * as queue from "./controllers/chatController.js";
-import cron from "node-cron";
-import dateFormat from "dateformat";
-var dateNow = dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
+import { existsSync, unlinkSync, readdir } from 'fs'
+import { join } from 'path'
+import makeWASocket, {
+    makeWALegacySocket,
+    useSingleFileAuthState,
+    useSingleFileLegacyAuthState,
+    makeInMemoryStore,
+    Browsers,
+    DisconnectReason,
+    delay,
+} from '@adiwajshing/baileys'
+import { toDataURL } from 'qrcode'
+import __dirname from './dirname.js'
+import response from './response.js'
+import db from './models/index.js'
+import * as queue from './controllers/chatController.js'
+import cron from 'node-cron'
+import dateFormat from 'dateformat'
+var dateNow = dateFormat(new Date(), 'yyyy-mm-dd hh:MM:ss')
 
-const Message = db.message;
-const Device = db.device;
-const MessageStatus = db.message_status;
+const Message = db.message
+const Device = db.device
+const MessageStatus = db.message_status
 
-const sessions = new Map();
-const retries = new Map();
+const sessions = new Map()
+const retries = new Map()
 
-const sessionsDir = (sessionId = "") => {
-  return join(__dirname, "sessions", sessionId ? `${sessionId}.json` : "");
-};
+const sessionsDir = (sessionId = '') => {
+    return join(__dirname, 'sessions', sessionId ? `${sessionId}.json` : '')
+}
 
 const isSessionExists = (sessionId) => {
-  return sessions.has(sessionId);
-};
+    return sessions.has(sessionId)
+}
 
 const isSessionFileExists = (name) => {
-  return existsSync(sessionsDir(name));
-};
+    return existsSync(sessionsDir(name))
+}
 
 const shouldReconnect = (sessionId) => {
-  let maxRetries = parseInt(process.env.MAX_RETRIES ?? 0);
-  let attempts = retries.get(sessionId) ?? 0;
+    let maxRetries = parseInt(process.env.MAX_RETRIES ?? 0)
+    let attempts = retries.get(sessionId) ?? 0
 
-  maxRetries = maxRetries < 1 ? 1 : maxRetries;
+    maxRetries = maxRetries < 1 ? 1 : maxRetries
 
-  if (attempts < maxRetries) {
-    ++attempts;
+    if (attempts < maxRetries) {
+        ++attempts
 
-    console.log("Reconnecting...", { attempts, sessionId });
-    retries.set(sessionId, attempts);
+        console.log('Reconnecting...', { attempts, sessionId })
+        retries.set(sessionId, attempts)
 
-    return true;
-  }
-
-  return false;
-};
-
-const createSession = async (sessionId, isLegacy = false, res = null) => {
-  const sessionFile = (isLegacy ? "legacy_" : "md_") + sessionId;
-  console.log(sessionId);
-  const store = makeInMemoryStore({});
-  const { state, saveState } = isLegacy ? useSingleFileLegacyAuthState(sessionsDir(sessionFile)) : useSingleFileAuthState(sessionsDir(sessionFile));
-
-  /**
-   * @type {(import('@adiwajshing/baileys').LegacySocketConfig|import('@adiwajshing/baileys').SocketConfig)}
-   */
-  const waConfig = {
-    auth: state,
-    printQRInTerminal: true,
-    // browser: Browsers.ubuntu("Chrome"),
-    browser: ["Eazynotif Multi Device", "MacOS", "3.0"],
-  };
-
-  /**
-   * @type {import('@adiwajshing/baileys').AnyWASocket}
-   */
-  const wa = isLegacy ? makeWALegacySocket(waConfig) : makeWASocket.default(waConfig);
-
-  if (!isLegacy) {
-    store.readFromFile(sessionsDir(`${sessionId}_store`));
-    store.bind(wa.ev);
-  }
-
-  sessions.set(sessionId, { ...wa, store, isLegacy });
-
-  wa.ev.on("creds.update", saveState);
-
-  // wa.ev.on('chats.update', m => console.log(m))
-  wa.ev.on("messages.update", async (chat) => {
-    if (chat.length > 0) {
-      const msg = chat[0];
-      if (msg.key.fromMe) {
-        var message_id = msg.key.id;
-        var status_message = msg.update.status;
-        updateStatusChat(message_id, status_message);
-      }
-    }
-  });
-
-  wa.ev.on("chats.set", ({ chats }) => {
-    if (isLegacy) {
-      store.chats.insertIfAbsent(...chats);
-    }
-  });
-
-  wa.ev.on("connection.update", async (update) => {
-    console.log("UPDATE CONNECTION : " + sessionId);
-    // console.log(update);
-    if (update.receivedPendingNotifications) {
-      updateDisconected(sessionId, true);
-
-      var newcon = `con${sessionId}`;
-
-      // newcon = cron.schedule("*/30 * * * * *", () => {
-      //   console.log("RUNNING CRON : " + sessionId);
-      //   queue.sendMessageQueue(sessionId);
-      // });
-      // upsertData(wa, isLegacy);
-    }
-    const { connection, lastDisconnect } = update;
-    const statusCode = lastDisconnect?.error?.output?.statusCode;
-
-    if (connection === "open") {
-      retries.delete(sessionId);
+        return true
     }
 
-    if (connection === "close") {
-      if (statusCode === DisconnectReason.loggedOut || !shouldReconnect(sessionId)) {
-        if (res && !res.headersSent) {
-          response(res, 500, false, "Unable to create session.");
+    return false
+}
+
+const talicreateSession = async (sessionId, isLegacy = false, res = null) => {
+    const sessionFile = (isLegacy ? 'legacy_' : 'md_') + sessionId
+    console.log(sessionId)
+    const store = makeInMemoryStore({})
+    const { state, saveState } = isLegacy
+        ? useSingleFileLegacyAuthState(sessionsDir(sessionFile))
+        : useSingleFileAuthState(sessionsDir(sessionFile))
+
+    /**
+     * @type {(import('@adiwajshing/baileys').LegacySocketConfig|import('@adiwajshing/baileys').SocketConfig)}
+     */
+    const waConfig = {
+        auth: state,
+        printQRInTerminal: true,
+        // browser: Browsers.ubuntu("Chrome"),
+        browser: ['Wa Sender', 'MacOS', '3.0'],
+    }
+
+    /**
+     * @type {import('@adiwajshing/baileys').AnyWASocket}
+     */
+    const wa = isLegacy ? makeWALegacySocket(waConfig) : makeWASocket.default(waConfig)
+
+    if (!isLegacy) {
+        store.readFromFile(sessionsDir(`${sessionId}_store`))
+        store.bind(wa.ev)
+    }
+
+    sessions.set(sessionId, { ...wa, store, isLegacy })
+
+    wa.ev.on('creds.update', saveState)
+
+    // wa.ev.on('chats.update', m => console.log(m))
+    wa.ev.on('messages.update', async (chat) => {
+        if (chat.length > 0) {
+            const msg = chat[0]
+            if (msg.key.fromMe) {
+                var message_id = msg.key.id
+                var status_message = msg.update.status
+                updateStatusChat(message_id, status_message)
+            }
+        }
+    })
+
+    wa.ev.on('chats.set', ({ chats }) => {
+        if (isLegacy) {
+            store.chats.insertIfAbsent(...chats)
+        }
+    })
+
+    wa.ev.on('connection.update', async (update) => {
+        console.log('UPDATE CONNECTION : ' + sessionId)
+        // console.log(update);
+        if (update.receivedPendingNotifications) {
+            updateDisconected(sessionId, true)
+
+            var newcon = `con${sessionId}`
+
+            // newcon = cron.schedule("*/30 * * * * *", () => {
+            //   console.log("RUNNING CRON : " + sessionId);
+            //   queue.sendMessageQueue(sessionId);
+            // });
+            // upsertData(wa, isLegacy);
+        }
+        const { connection, lastDisconnect } = update
+        const statusCode = lastDisconnect?.error?.output?.statusCode
+
+        if (connection === 'open') {
+            retries.delete(sessionId)
         }
 
-        return deleteSession(sessionId, isLegacy);
-      }
-      if (statusCode !== DisconnectReason.loggedOut) {
-        createSession(sessionId, isLegacy, res);
-      }
+        if (connection === 'close') {
+            if (statusCode === DisconnectReason.loggedOut || !shouldReconnect(sessionId)) {
+                if (res && !res.headersSent) {
+                    response(res, 500, false, 'Unable to create session.')
+                }
 
-      // setTimeout(
-      //   () => {
-      //     createSession(sessionId, isLegacy, res);
-      //   },
-      //   statusCode === DisconnectReason.restartRequired
-      //     ? 0
-      //     : parseInt(process.env.RECONNECT_INTERVAL ?? 0)
-      // );
-      //"level":40,"time":"2022-05-10T03:13:35.617Z","pid":36063,"hostname":"server.eazynotif.id","class":"baileys","msg":"If your process stalls here, make sure to implement the reconnect logic as shown in https://github.com/adiwajshing/Baileys/blob/master/Example/example.ts#:~:text=reconnect"}
-    }
+                return deleteSession(sessionId, isLegacy)
+            }
+            if (statusCode !== DisconnectReason.loggedOut) {
+                createSession(sessionId, isLegacy, res)
+            }
 
-    if (update.qr) {
-      if (res && !res.headersSent) {
-        try {
-          const qr = await toDataURL(update.qr);
-
-          response(res, 200, true, "QR code received, please scan the QR code.", { qr });
-        } catch {
-          response(res, 500, false, "Unable to create QR code.");
+            // setTimeout(
+            //   () => {
+            //     createSession(sessionId, isLegacy, res);
+            //   },
+            //   statusCode === DisconnectReason.restartRequired
+            //     ? 0
+            //     : parseInt(process.env.RECONNECT_INTERVAL ?? 0)
+            // );
+            //"level":40,"time":"2022-05-10T03:13:35.617Z","pid":36063,"hostname":"server.eazynotif.id","class":"baileys","msg":"If your process stalls here, make sure to implement the reconnect logic as shown in https://github.com/adiwajshing/Baileys/blob/master/Example/example.ts#:~:text=reconnect"}
         }
 
-        return;
-      }
+        if (update.qr) {
+            if (res && !res.headersSent) {
+                try {
+                    const qr = await toDataURL(update.qr)
 
-      try {
-        await wa.logout();
-      } catch {
-      } finally {
-        deleteSession(sessionId, isLegacy);
-      }
-    }
-  });
-};
+                    response(res, 200, true, 'QR code received, please scan the QR code.', { qr })
+                } catch {
+                    response(res, 500, false, 'Unable to create QR code.')
+                }
+
+                return
+            }
+
+            try {
+                await wa.logout()
+            } catch {
+            } finally {
+                deleteSession(sessionId, isLegacy)
+            }
+        }
+    })
+}
 
 const upsertData = (wa, isLegacy) => {
-  wa.ev.on("messages.upsert", async (m) => {
-    const message = m.messages[0];
+    wa.ev.on('messages.upsert', async (m) => {
+        const message = m.messages[0]
 
-    if (!message.key.fromMe && m.type === "notify") {
-      await delay(1000);
+        if (!message.key.fromMe && m.type === 'notify') {
+            await delay(1000)
 
-      if (isLegacy) {
-        await wa.chatRead(message.key, 1);
-      } else {
-        await wa.sendReadReceipt(message.key.remoteJid, message.key.participant, [message.key.id]);
-      }
-    }
-  });
-};
+            if (isLegacy) {
+                await wa.chatRead(message.key, 1)
+            } else {
+                await wa.sendReadReceipt(message.key.remoteJid, message.key.participant, [message.key.id])
+            }
+        }
+    })
+}
 
 /**
  * @returns {(import('@adiwajshing/baileys').AnyWASocket|null)}
  */
 const getSession = (sessionId) => {
-  return sessions.get(sessionId) ?? null;
-};
+    return sessions.get(sessionId) ?? null
+}
 
 const deleteSession = (sessionId, isLegacy = false) => {
-  console.log("deleteSession : " + sessionId);
-  const sessionFile = (isLegacy ? "legacy_" : "md_") + sessionId;
-  const storeFile = `${sessionId}_store`;
+    console.log('deleteSession : ' + sessionId)
+    const sessionFile = (isLegacy ? 'legacy_' : 'md_') + sessionId
+    const storeFile = `${sessionId}_store`
 
-  if (isSessionFileExists(sessionFile)) {
-    unlinkSync(sessionsDir(sessionFile));
-  }
+    if (isSessionFileExists(sessionFile)) {
+        unlinkSync(sessionsDir(sessionFile))
+    }
 
-  if (isSessionFileExists(storeFile)) {
-    unlinkSync(sessionsDir(storeFile));
-  }
+    if (isSessionFileExists(storeFile)) {
+        unlinkSync(sessionsDir(storeFile))
+    }
 
-  sessions.delete(sessionId);
-  retries.delete(sessionId);
-  updateDisconected(sessionId, false);
-};
+    sessions.delete(sessionId)
+    retries.delete(sessionId)
+    updateDisconected(sessionId, false)
+}
 
 const getChatList = (sessionId, isGroup = false) => {
-  const filter = isGroup ? "@g.us" : "@s.whatsapp.net";
+    const filter = isGroup ? '@g.us' : '@s.whatsapp.net'
 
-  return getSession(sessionId).store.chats;
-};
+    return getSession(sessionId).store.chats
+}
 
 /**
  * @param {import('@adiwajshing/baileys').AnyWASocket} session
  */
 const isExists = async (session, jid, isGroup = false) => {
-  try {
-    let result;
+    try {
+        let result
 
-    if (isGroup) {
-      result = await session.groupMetadata(jid);
+        if (isGroup) {
+            result = await session.groupMetadata(jid)
 
-      return Boolean(result.id);
+            return Boolean(result.id)
+        }
+
+        if (session.isLegacy) {
+            result = await session.onWhatsApp(jid)
+        } else {
+            ;[result] = await session.onWhatsApp(jid)
+        }
+
+        return result.exists
+    } catch {
+        return false
     }
-
-    if (session.isLegacy) {
-      result = await session.onWhatsApp(jid);
-    } else {
-      [result] = await session.onWhatsApp(jid);
-    }
-
-    return result.exists;
-  } catch {
-    return false;
-  }
-};
+}
 
 /**
  * @param {import('@adiwajshing/baileys').AnyWASocket} session
  */
 const sendMessage = async (session, receiver, message) => {
-  try {
-    await delay(1000);
+    try {
+        await delay(1000)
 
-    return session.sendMessage(receiver, message);
-  } catch {
-    return Promise.reject(null); // eslint-disable-line prefer-promise-reject-errors
-  }
-};
+        return session.sendMessage(receiver, message)
+    } catch {
+        return Promise.reject(null) // eslint-disable-line prefer-promise-reject-errors
+    }
+}
 
 const formatPhone = (phone) => {
-  if (phone.endsWith("@s.whatsapp.net")) {
-    return phone;
-  }
+    if (phone.endsWith('@s.whatsapp.net')) {
+        return phone
+    }
 
-  let formatted = phone.replace(/\D/g, "");
+    let formatted = phone.replace(/\D/g, '')
 
-  return (formatted += "@s.whatsapp.net");
-};
+    return (formatted += '@s.whatsapp.net')
+}
 
 const formatGroup = (group) => {
-  if (group.endsWith("@g.us")) {
-    return group;
-  }
+    if (group.endsWith('@g.us')) {
+        return group
+    }
 
-  let formatted = group.replace(/[^\d-]/g, "");
+    let formatted = group.replace(/[^\d-]/g, '')
 
-  return (formatted += "@g.us");
-};
+    return (formatted += '@g.us')
+}
 
 const cleanup = () => {
-  console.log("Running cleanup before exit.");
+    console.log('Running cleanup before exit.')
 
-  sessions.forEach((session, sessionId) => {
-    if (!session.isLegacy) {
-      session.store.writeToFile(sessionsDir(`${sessionId}_store`));
-    }
-  });
-};
+    sessions.forEach((session, sessionId) => {
+        if (!session.isLegacy) {
+            session.store.writeToFile(sessionsDir(`${sessionId}_store`))
+        }
+    })
+}
 
 const init = () => {
-  readdir(sessionsDir(), (err, files) => {
-    if (err) {
-      throw err;
-    }
+    readdir(sessionsDir(), (err, files) => {
+        if (err) {
+            throw err
+        }
 
-    for (const file of files) {
-      if (!file.endsWith(".json") || (!file.startsWith("md_") && !file.startsWith("legacy_")) || file.includes("_store")) {
-        continue;
-      }
+        for (const file of files) {
+            if (
+                !file.endsWith('.json') ||
+                (!file.startsWith('md_') && !file.startsWith('legacy_')) ||
+                file.includes('_store')
+            ) {
+                continue
+            }
 
-      const filename = file.replace(".json", "");
-      const isLegacy = filename.split("_", 1)[0] !== "md";
-      const sessionId = filename.substring(isLegacy ? 7 : 3);
+            const filename = file.replace('.json', '')
+            const isLegacy = filename.split('_', 1)[0] !== 'md'
+            const sessionId = filename.substring(isLegacy ? 7 : 3)
 
-      createSession(sessionId, isLegacy);
+            createSession(sessionId, isLegacy)
 
-      // var newcon = `con${sessionId}`;
-      // newcon = cron.schedule('*/10 * * * * *', () => {
-      //     console.log('RUNNING CRON : ' + sessionId);
-      //     queue.sendMessageQueue(sessionId);
-      // });
-    }
-  });
-};
+            // var newcon = `con${sessionId}`;
+            // newcon = cron.schedule('*/10 * * * * *', () => {
+            //     console.log('RUNNING CRON : ' + sessionId);
+            //     queue.sendMessageQueue(sessionId);
+            // });
+        }
+    })
+}
 
-init();
+init()
 
 function updateDisconected(device_id, status) {
-  let values = {
-    status: status ? "true" : "false",
-    last_status_update: dateNow,
-  };
-  Device.update(values, { where: { device_id: device_id } });
+    let values = {
+        status: status ? 'true' : 'false',
+        last_status_update: dateNow,
+    }
+    Device.update(values, { where: { device_id: device_id } })
 }
 
 function updateStatusChat(message_id, status_message) {
-  let values = { status_message: status_message };
-  Message.update(values, { where: { message_id: message_id } });
-  MessageStatus.findOne({
-    where: { message_id: message_id, status: status_message },
-  }).then((res) => {
-    if (!res) {
-      MessageStatus.create({ message_id: message_id, status: status_message });
-    }
-  });
+    let values = { status_message: status_message }
+    Message.update(values, { where: { message_id: message_id } })
+    MessageStatus.findOne({
+        where: { message_id: message_id, status: status_message },
+    }).then((res) => {
+        if (!res) {
+            MessageStatus.create({ message_id: message_id, status: status_message })
+        }
+    })
 }
 
-export { isSessionExists, createSession, getSession, deleteSession, getChatList, isExists, sendMessage, formatPhone, formatGroup, cleanup, init };
+export {
+    isSessionExists,
+    createSession,
+    getSession,
+    deleteSession,
+    getChatList,
+    isExists,
+    sendMessage,
+    formatPhone,
+    formatGroup,
+    cleanup,
+    init,
+}
