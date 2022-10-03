db250b8cb4f7cfa079d0f10340c22c0b3afb1e2b
Febriansyah <febriansyah032@gmail.com>
2022-10-03 21:14:08 +0700

    update git

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import sessionRoutes from './routes/sessionRoutes.js'
 import chatRoutes from './routes/chatRoutes.js'
+import chatDirectRoutes from './routes/chatDirectRoutes.js'
 import groupRoutes from './routes/groupRoutes.js'
 import response from './response.js'
 
@@ -8,6 +9,7 @@ const router = Router()
 
 router.use('/session', sessionRoutes)
 router.use('', chatRoutes)
+router.use('', chatDirectRoutes)
 router.use('/group', groupRoutes)
 
 router.all('*', (req, res) => {
