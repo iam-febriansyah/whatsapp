cf5d5c79074c811874969d2cabe05456b70c4d43
Febriansyah <febriansyah032@gmail.com>
2022-10-04 00:27:14 +0700

    update git

--- routes/checkRoutes.js
+++ routes/checkRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import * as controller from '../controllers/checkController'
+import * as controller from '../controllers/checkController.js'
 
 const router = Router()
 
